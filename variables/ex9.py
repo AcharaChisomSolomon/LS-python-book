bcd1a3e60430dd46d4651c544caf52b08a74c688
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-09-15 11:02:05 +0000

    exercise 9 done

---
+++ variables/ex9.py
@@ -0,0 +1,17 @@
+balance = 1000
+rate = 0.05
+
+balance = balance + balance * rate
+print(f'After 1 year, your balance will be {balance}.')
+
+balance = balance + balance * rate
+print(f'After 2 years, your balance will be {balance}.')
+
+balance = balance + balance * rate
+print(f'After 3 years, your balance will be {balance}.')
+
+balance = balance + balance * rate
+print(f'After 4 years, your balance will be {balance}.')
+
+balance = balance + balance * rate
+print(f'After 5 years, your balance will be {balance}.')
