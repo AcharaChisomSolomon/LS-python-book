bab16d2d57c4d17a5022e09a1ca1371dee8f7600
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-09-15 10:59:16 +0000

    age exercise done

---
+++ variables/ex6.py
@@ -0,0 +1,6 @@
+age = 22
+
+print(f'In 10 years, I will be {age + 10} years old.')
+print(f'In 20 years, I will be {age + 20} years old.')
+print(f'In 30 years, I will be {age + 30} years old.')
+print(f'In 40 years, I will be {age + 40} years old.')
