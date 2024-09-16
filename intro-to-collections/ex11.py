c1740cf412e922efbfc181e6058dff8c4a1f8f91
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-09-16 10:26:17 +0000

    exercise 11 done

---
+++ intro-to-collections/ex11.py
@@ -0,0 +1,11 @@
+name_and_country = {
+    'Alice': 'USA',
+    'Francois': 'Canada',
+    'Inti': 'Peru',
+    'Monica': 'Germany',
+    'Sanyu': 'Uganda',
+    'Yoshitaka': 'Japan'
+}
+
+print(name_and_country['Alice'])
+print(name_and_country['Francois'])
